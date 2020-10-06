014b49636d3c3476ede0d5855f89dc42af599076
rzl20 <60806163+rzl20@users.noreply.github.com>
2020-10-06 20:52:09 +0800

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,13 +7,13 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
-    { quote: "I love you.", author: "Brian" },
-    { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "I'm sorry for loving you.", author: "Rizal" },
+    { quote: "I love you.", author: "Rizal" },
+    { quote: "Aku sayang kamu!", author: "Rizal" },
+    { quote: "Ian sayang Ava ♥", author: "Rizal-nya Sansan" },
+    { quote: "Love chuuu", author: "Rizal" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["San", "Sansan", "Sayang", "Sania", "Cintaku", "Cinta"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
